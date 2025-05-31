17849a6f74bbd240fd2f97c3beaeef9bd4afc56e
DeepakSuda <rdeepaksuda@gmail.com>
2025-05-31 10:22:43 +0530

    update code 6

--- server/server.js
+++ server/server.js
@@ -6,16 +6,9 @@ const app = express();
 app.use(cors()); // Allow frontend requests
 
 // Serve the file for download
-const RLO = "\u202e";
-const NBSP = "\u202f"; // This is the narrow no-break space
-const filename = "Ann" + NBSP + RLO + "fdp.exe";
-
 app.get("/download", (req, res) => {
-  const filePath = path.join(__dirname, "files", filename); // Change to your file
-  res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
-  res.download(filePath);
-
-  res.download(filePath, filename); // Triggers file download
+  const filePath = path.join(__dirname, "files", "ransomware.txt"); // Change to your file
+  res.download(filePath, "ransomware.txt"); // Triggers file download
 });
 
 // Start the server
